2f680eada0f191ce5b667cb8c096b8b9ff72e561
Chester Harvey <chesterharvey@gmail.com>
2023-11-17 12:24:08 -0700

    model updates

--- Modeling/chester/modeling.py
+++ Modeling/chester/modeling.py
@@ -4,35 +4,35 @@ from spint.gravity import  BaseGravity, Gravity, Production, Attraction, Doubly
 import statsmodels.api as sm
 from utils import *
 
-def inputs(df, flows, o_vars, d_vars, od_vars, cost):
+def inputs(df, flows, o_vars, d_vars, od_vars):
     flows = prep_col(flows, df)
     o_vars = prep_cols(o_vars, df)
-    d_vars = prep_cols(d_vars + od_vars, df)
-    cost = prep_col(cost, df)
-    return flows, o_vars, d_vars, cost
+    d_vars = prep_cols(d_vars, df)
+    od_vars = prep_cols(od_vars, df)
+    return flows, o_vars, d_vars, od_vars
 
-def gravity(df, flows, o_vars, d_vars, od_vars, cost, constant=True):
+def gravity(df, flows, o_vars, d_vars, od_vars, constant=True):
     if constant:
-        covars = ['const'] + o_vars + d_vars + od_vars + [cost]
+        covars = ['const'] + o_vars + d_vars + od_vars
     else:
-        covars = o_vars + d_vars + od_vars + [cost]
-    flows, o_vars, d_vars, cost = inputs(df, flows, o_vars, d_vars, od_vars, cost)
-    model = Gravity(flows, o_vars, d_vars, cost, 'exp', constant=constant)
+        covars = o_vars + d_vars + od_vars
+    flows, o_vars, d_vars, od_vars = inputs(df, flows, o_vars, d_vars, od_vars)
+    model = Gravity(flows, o_vars, d_vars, od_vars, 'exp', constant=constant)
     summary = summarize_gravity_model(model, covars)
     return model, summary
 
-def ols(df, flows, o_vars, d_vars, od_vars, cost, constant=True):
+def ols(df, flows, o_vars, d_vars, od_vars, constant=True):
     Y = df[flows]
-    X = df[o_vars + d_vars + od_vars + [cost]]
+    X = df[o_vars + d_vars + od_vars]
     if constant:
         X = sm.add_constant(X)
     model = sm.OLS(Y,X).fit()
     summary = summarize_ols_model(model)
     return model, summary
 
-def estimate_models(df, flows, o_vars, d_vars, od_vars, cost, constant=True):
-    grav_model, grav_summary = gravity(df, flows, o_vars, d_vars, od_vars, cost, constant=constant)
-    ols_model, ols_summary = ols(df, flows, o_vars, d_vars, od_vars, cost, constant=constant)
+def estimate_models(df, flows, o_vars, d_vars, od_vars, constant=True):
+    grav_model, grav_summary = gravity(df, flows, o_vars, d_vars, od_vars, constant=constant)
+    ols_model, ols_summary = ols(df, flows, o_vars, d_vars, od_vars, constant=constant)
     df = combine_model_summaries(
         [grav_summary, ols_summary],
         ['Gravity', 'OLS'])
